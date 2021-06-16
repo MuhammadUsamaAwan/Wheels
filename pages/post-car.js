8ef7f0bbef80419a48ab776cc386ef3128203aa2
MuhammadUsamaAwan <underusama@gmail.com>
2021-06-16 10:14:49 +0500

    post ad new fields

--- pages/post-car.js
+++ pages/post-car.js
@@ -8,12 +8,14 @@ import {
   Button,
   Cascader,
   DatePicker,
+  Checkbox,
 } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { getManufacturers } from "../redux/actions/manufacturer";
 import LoadingIcon from "../components/UI/LoadingIcon";
 import UploadPics from "../components/UI/UploadPics";
 import { postAd } from "../service/service";
+import moment from 'moment';
 
 const { Option, OptGroup } = Select;
 const layout = {
@@ -42,21 +44,24 @@ const postCar = () => {
       value: make.title,
       label: make.title,
       children: make.model.map(model => ({
-        value: model.title,
+        value: model._id,
         label: model.title,
       })),
     }));
   }
 
-  const onFinish = () => {
+  const otherFeatures = ['ABS', 'Air Bags', 'Air Conditioning', 'Alloy Rims', 'AM/FM Radio', 'CD Player', 'Cassette Player', 'Cool Box', 'Cruise Control', 'Climate Control', 'DVD Player', 'Front Speakers', 'Front Camera', 'Heated Seats', 'Immobilizer Key', 'Keyless Entry', 'Navigation System', 'Power Locks', 'Power Mirrors', 'Power Steering', 'Power Windows', 'Rear Seat Entertainment', 'Rear AC Vents', 'Rear Speakers', 'Rear Camera', 'Sun Roof', 'Steering Switches', 'USB and Auxillary Cable']
+
+  const onFinish = (values) => {
     const images = {
       images: img,
     };
-    values.push(images);
-    const vehicleType = {
-      vehicleType: "car",
-    };
-    values.push(vehicleType);
+    values = {...values, images}
+    values = {...values, vehicleType: 'car'}
+    values.modelId = values.modelId[1];
+    if(values.other) {
+    values.other = values.other.join(" ");
+    }
     postAd(token, values);
     console.log(values);
   };
@@ -76,11 +81,36 @@ const postCar = () => {
             label="Title"
             name="title"
             rules={[
-              { required: true, message: "Please input your car's title!" },
+              { required: true, message: "Please input your advertisement's title!" },
             ]}
             required
           >
-            <Input />
+            <Input type="text" />
+          </Form.Item>
+
+          <Form.Item label="Country" name="country" rules={[
+              {
+                required: true,
+                message: "Please input your car's country!",
+              },
+            ]}>
+            <Select>
+                <Option value="Pakistan">Pakistan</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="State" name="state" rules={[
+              {
+                required: true,
+                message: "Please input your car's state!",
+              },
+            ]}>
+            <Select>
+                <Option value="Punjab">Punjab</Option>
+                <Option value="Sindh">Sindh</Option>
+                <Option value="Khyber Pakhtunkhwa">Khyber Pakhtunkhwa</Option>
+                <Option value="Balochistan">Balochistan</Option>
+            </Select>
           </Form.Item>
 
           <Form.Item
@@ -106,7 +136,7 @@ const postCar = () => {
 
           <Form.Item
             label="Car Info"
-            name="title"
+            name="modelId"
             rules={[
               { required: true, message: "Please input your car's info!" },
             ]}
@@ -114,7 +144,7 @@ const postCar = () => {
           >
             <Cascader options={options} />
           </Form.Item>
-
+          
           <Form.Item label="Registration City" name="registrationCity">
             <Select>
               <OptGroup label="Popular Cities">
@@ -140,7 +170,7 @@ const postCar = () => {
             ]}
             required
           >
-            <DatePicker picker="year" />
+            <Input type="number" />
           </Form.Item>
 
           <Form.Item
@@ -247,7 +277,43 @@ const postCar = () => {
           >
             <Input.TextArea rows={4} />
           </Form.Item>
+
+          <Form.Item
+            label="Engine Capacity"
+            name="power"
+          >
+            <Input type="number" addonAfter="CC" />
+          </Form.Item>
+
+          <Form.Item
+            label="Transmission"
+            name="transmission"
+          >
+            <Select>
+              <Option value="Auto">Auto</Option>
+              <Option value="Manual">Manual</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Body Type"
+            name="bodyType"
+          >
+            <Select>
+              <Option value="Hatchback">Hatchback</Option>
+              <Option value="Sedan">Sedan</Option>
+              <Option value="SUV">SUV</Option>
+              <Option value="Pajero">Pajero</Option>
+            </Select>
+          </Form.Item>
         </Card>
+
+        <Form.Item
+            label="Features"
+            name="other"
+          >
+        <Checkbox.Group options={otherFeatures} onChange = {(e) => console.log(e.target)}/>
+        </Form.Item>
 
         <Card style={{ marginTop: "2rem" }}>
           <Typography.Title level={3}>Upload Photos</Typography.Title>
@@ -272,7 +338,7 @@ const postCar = () => {
         </Card>
 
         <div style={{ marginTop: "2rem" }}>
-          <Button type="primary" block>
+          <Button type="primary"  htmlType="submit" block>
             Submit & Continue
           </Button>
         </div>
